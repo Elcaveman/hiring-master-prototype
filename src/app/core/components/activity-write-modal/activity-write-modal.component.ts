ac3cf63029915f75f735090bea68d5f99698cc76
El mehdi <elmehdi.oudaoud@zsoft-consulting.com>
2023-02-01 12:26:26 +0100

    App - init ativity write component

---
+++ src/app/core/components/activity-write-modal/activity-write-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-write-modal',
+  templateUrl: './activity-write-modal.component.html',
+  styleUrls: ['./activity-write-modal.component.scss']
+})
+export class ActivityWriteModalComponent {
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivityWriteModalComponent } from './components/data/activity-write-modal/activity-write-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ActivityWriteModalComponent
+  ],
   imports: [
     CommonModule
   ]
