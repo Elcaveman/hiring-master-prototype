0d42b0d6c97660ff87ae217dc84be461f8dbe6ec
El mehdi <elmehdi.oudaoud@zsoft-consulting.com>
2023-02-02 10:51:04 +0100

    Top Nav - data entries

--- src/app/core/components/top-navbar/top-navbar.component.ts
+++ src/app/core/components/top-navbar/top-navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TopNavDisplayModel } from './top-navbar.models';
+import { FakeDataService } from '../../services/fake-data.service';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-top-navbar',
@@ -6,15 +9,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./top-navbar.component.scss']
 })
 export class TopNavbarComponent {
-  isCollapsed:boolean;
   @Input() collapsed :boolean=false;// initial state
   @Output() collapseEvent = new EventEmitter<boolean>;
-  constructor(){
+  isCollapsed:boolean;
+  displayData ?: Observable<TopNavDisplayModel>;
+  constructor(private fakeDataService_:FakeDataService){
     this.isCollapsed = this.collapsed;
+    this.displayData = fakeDataService_.titleData().pipe(
+      map(x => new TopNavDisplayModel(x.title,x.state))
+    )
   }
   triggerCollapseEvent($event:MouseEvent){
-    console.log("triggerCollapseEvent",$event)
     this.isCollapsed = !this.isCollapsed
     this.collapseEvent.emit(this.isCollapsed)
   }
+  
 }
---
+++ src/app/core/components/top-navbar/top-navbar.models.ts
@@ -0,0 +1,10 @@
+enum STATE_ENUM { ACTIVE, INNACTIVE}
+
+export class TopNavDisplayModel{
+    title: string=""; // can change
+    state:STATE_ENUM=STATE_ENUM.ACTIVE; // can change
+    constructor(title:string,state:string){
+        this.title = title;
+        (state=="active")?STATE_ENUM.ACTIVE:STATE_ENUM.INNACTIVE
+    }
+}
