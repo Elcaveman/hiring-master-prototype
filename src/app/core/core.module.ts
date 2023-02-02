aa504434ddfa3e7a194ea9112c86db37b925cac8
El mehdi <elmehdi.oudaoud@zsoft-consulting.com>
2023-02-02 16:52:52 +0100

    Top nav - template updates

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,8 @@ import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,10 +16,12 @@ import { SideNavbarComponent } from './components/side-navbar/side-navbar.compon
     SideNavbarComponent
   ],
   imports: [
+    RouterModule,
     SharedModule,
     NzAutocompleteModule,
     NzInputModule,
-    NzAvatarModule
+    NzAvatarModule,
+    NzBadgeModule
   ],
   exports:[
     ActivityWriteModalComponent,
