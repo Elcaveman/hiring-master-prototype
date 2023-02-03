ae6ee5399fe5c95ff3af6ca3c4b01839fc314c8d
El mehdi <elmehdi.oudaoud@zsoft-consulting.com>
2023-02-03 14:28:49 +0100

    App - default map

--- src/app/core/utilities/safeMap.ts
+++ src/app/core/utilities/safeMap.ts
@@ -1,24 +1,14 @@
-export class SafeMap<Key,Value>{
-    private inner_map: Map<Key,Value> = new Map<Key,Value>();
+export class SafeMap<Key,Value> extends Map<Key,Value>{
     private default_fallback: Value;
     constructor(default_fallback:Value){
+        super();
         this.default_fallback = default_fallback;
     }
-    get(key:Key):Value{
+    override get(key:Key):Value{
         if (this.default_fallback == undefined) {
             throw new Error('No default_fallback defined');
         }
         /* Type assertion to Ignore compiler warning since we handle the undefined case manually */
-        else return (this.inner_map.has(key))?this.inner_map.get(key)!:this.default_fallback;
+        else return (super.has(key))?super.get(key)!:this.default_fallback;
     }
-    set(key:Key,value:Value){
-        this.inner_map.set(key,value);
-    }
-    delete(key:Key){
-        return this.inner_map.delete(key)
-    }
-    clear(){
-        this.inner_map.clear()
-    }
-    /* Todo impelment loop */
 }
