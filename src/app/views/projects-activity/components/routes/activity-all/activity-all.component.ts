62ce6b1abd0e3c67650b0764d24e2b7ccee15be8
El mehdi <elmehdi.oudaoud@zsoft-consulting.com>
2023-02-10 11:53:23 +0100

    Activity - rounded time display

--- src/app/views/projects-activity/components/routes/activity-all/activity-all.component.ts
+++ src/app/views/projects-activity/components/routes/activity-all/activity-all.component.ts
@@ -17,8 +17,8 @@ export class ActivityAllComponent implements OnInit,OnDestroy {
   START_TIME = 8;
   END_TIME = 22;
   MINUTES_STEP = 15;
-  timeOptions:{hours:number[],display:string}[] = this.timeRange(this.START_TIME,this.END_TIME,this.MINUTES_STEP);
-  selectedTime:Map<number,{hours:number[],display:string}>=new Map();
+  timeOptions:{time:number[],display:string}[] = this.timeRange(this.START_TIME,this.END_TIME,this.MINUTES_STEP);
+  selectedTime:Map<number,{time:number[],display:string}>=new Map();
   // private fake_data : (Interview | Reminder | Reunion | Task )[] = [
   //   new Interview(1,"interview 1"),new Interview(2,"interview 2"),
   //   new Interview(3,"interview 1"),new Reunion(4,"Reunion 2"),
@@ -84,20 +84,20 @@ export class ActivityAllComponent implements OnInit,OnDestroy {
   REUNION_MEDIUM = REUNION_MEDIUM;
   TASK_MEDIUM = TASK_MEDIUM;
 
-  timeRange(start:PositiveNumber,end:PositiveNumber,minutes_step:PositiveNumber):{hours:number[],display:string}[]{
-    const range:{hours:number[],display:string}[]= []
+  timeRange(start:PositiveNumber,end:PositiveNumber,minutes_step:PositiveNumber):{time:number[],display:string}[]{
+    const range:{time:number[],display:string}[]= []
     if (start<end){
       
       for (let hour=start;hour<end;hour++){
         for (let minutes=0;minutes<60;minutes+=minutes_step){
           range.push({
-            hours:[hour,minutes],
+            time:[hour,minutes],
             display:`${hour>=10?hour:'0'+hour}:${minutes>=10?minutes:'0'+minutes}`
           })
         }
       }
       range.push({
-        hours:[end,0],
+        time:[end,0],
         display:`${end>=10?end:'0'+end}:00`
       })
     }
@@ -167,18 +167,23 @@ export class ActivityAllComponent implements OnInit,OnDestroy {
   approximate(
     origin_date:Date,
     START_TIME: PositiveNumber,END_TIME: PositiveNumber
-    ) :{hours:number[],display:string}{
+    ) :{time:number[],display:string}{
 
       const [hours,minutes] = [origin_date.getHours(),origin_date.getMinutes()]
-      if (hours<START_TIME) return {hours:[START_TIME,0],display:`${START_TIME>=10?START_TIME:'0'+START_TIME}:00`}
-      else if (hours>END_TIME) return {hours:[END_TIME,0],display:`${END_TIME>=10?END_TIME:'0'+END_TIME}:00`}
+      if (hours<START_TIME) return {time:[START_TIME,0],display:`${START_TIME>=10?START_TIME:'0'+START_TIME}:00`}
+      else if (hours>END_TIME) return {time:[END_TIME,0],display:`${END_TIME>=10?END_TIME:'0'+END_TIME}:00`}
       else{
         const rounded_minutes = minutes - minutes%15;
         return {
-          hours:[hours,rounded_minutes],
+          time:[hours,rounded_minutes],
           display:`${hours>=10?hours:'0'+hours}:${rounded_minutes>=10?rounded_minutes:'0'+rounded_minutes}`
         }
       } 
+  }
+  compareSelectedTime(o1: any, o2: any):boolean {
+    if(o1!=null && o2!=null)
+      return (o1.time[0] == o2.time[0] && o1.time[1] == o2.time[1]);
+    return false;
   }
   ngOnDestroy(){
     this.ngUnsubscribe.next();
