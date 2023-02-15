406867a9de2a1b56efbf986d34c3254bd0b3fed9
El mehdi <elmehdi.oudaoud@zsoft-consulting.com>
2023-02-15 12:09:02 +0100

    Activity - custom date picker init

---
+++ src/app/views/projects-activity/components/data/custom-date-picker/custom-date-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-date-picker',
+  templateUrl: './custom-date-picker.component.html',
+  styleUrls: ['./custom-date-picker.component.scss']
+})
+export class CustomDatePickerComponent {
+
+}
